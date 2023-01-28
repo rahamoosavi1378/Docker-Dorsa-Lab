859ca939af1080c29dedb28f8de57d2a187b4ff6
rm <rahamoosavi1378@gmail.com>
2023-01-28 12:20:40 +0100

    git init

---
+++ centos/start.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/sh
+git clone https://github.com/meisamrce/DorsaLab.git /var/www/html/;
+rm -rf /var/www/html/.git;
+/usr/bin/sh
---
+++ lab/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST', 'sql');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'dorsalab');
+define('PROJECT_FOLDER', 'dorsalab');
+define('BASE_URL', 'http://localhost/');
+require_once  'helper/nusoap.php';
+require_once  'helper/helper.php';
+$main = new Helper();
